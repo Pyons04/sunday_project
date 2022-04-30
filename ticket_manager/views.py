434ad69b6b52883fa4dac2d8ad7ac7bb153452ca
pyons04 <broad.basic15gmail.com>
2022-04-30 17:52:49 +0900

    add ticket

--- ticket_manager/views.py
+++ ticket_manager/views.py
@@ -7,11 +7,25 @@ class TicketView(generic.ListView):
   model = Ticket
   template_name = 'list.html'
 
-class CreateTicketView(generic.FormView):
+class CreateTicketView(generic.CreateView):
   model = Ticket
   template_name = 'create.html'
 
   form_class = TicketCreateForm
+
+  def form_valid(self, form):
+    ticket = form.save(commit=True)
+    ticket.save()
+
+    logger = logging.getLogger('development')
+    logger.info('登録中')
+
+    messages.success(self.request, "登録完了")
+    return super().form_valid(form)
+
+  def form_invalid(self, form):
+    messages.error(self.request, "登録失敗")
+    return super().form_invalid(form)
 
 
 
