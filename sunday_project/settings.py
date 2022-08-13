826273d41dac9f3e196b958d864c5afb8917c2bf
pyons04 <broad.basic15gmail.com>
2022-08-13 11:57:44 +0900

    add truster origins for local vue server

--- sunday_project/settings.py
+++ sunday_project/settings.py
@@ -128,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL    = 'ticket_manager.User'
 LOGIN_REDIRECT_URL = '/ticket/kanban'
 LOGIN_URL          = '/ticket/login/'
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8080']
